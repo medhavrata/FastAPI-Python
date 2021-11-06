0198e3bfad5dbea2e705cfe539a96ffe310b150e
Medha <tyagi.25@gmail.com>
2021-11-06 18:49:58 +0000

    added posts path

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# @app -> This is the decorator and need to provide the instance name of FastAPI class, app, in this case
+# get -> Method name
+# "/" -> path
+# root -> Function, can use any name
+# Below funciton is for root of application
+
 @app.get("/")
 async def root():
     return {"message": "First fastAPI running"}
+
+
+# Write another function to retrieve the posts
+
+@app.get("/posts")
+def get_posts():
+    return{"message": "Here are your posts, enjoy your day"}
